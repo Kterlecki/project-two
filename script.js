f2b1ba6b12dfb9abf468c569eadde41e18f370b0
Kterlecki <ubuntu@ip-172-31-18-150.ec2.internal>
2019-10-02 11:31:40 +0000

    connected emailJS API and contact form small update

--- script.js
+++ script.js
@@ -68,3 +68,22 @@ function initMap() {
         }
     }
 }
+
+////---------------------email service
+
+function sendEmail(contactForm) {
+    emailjs.send('gmail', 'project_two',{
+        'from_name':contactForm.name.value,
+        'from_email': contactForm.emailaddress.value,
+        'restaurant_name':contactForm.name.value,
+        'restaurant_address':contactForm.name.value,
+        'project_request':contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log('Success',response);
+        },
+        function(error){
+            console.log('FAILED', error)
+        });
+}
